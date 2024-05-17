1013a1d947caa0ef481ea1c2d98d0e075f82c5f6
pedrolduarte13 <pedro.l.duarte@iesb.edu.br>
2024-05-17 20:59:36 -0300

    Departamentos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,28 +1,35 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const CargoController = require('../controllers/CargoController');
-const FuncionarioController = require('../controllers/FuncionarioController');
+const CargoController = require('../controllers/CargoController')
+const FuncionarioController = require('../controllers/FuncionarioController')
+const DepartamentoController = require('../controllers/DepartamentoController')
 
 // validators
-const { validarID } = require('../validators/idValidator');
-const { validarCargo } = require('../validators/CargoValidator');
-const { validarFuncionario } = require('../validators/FuncionarioValidator');
+const { validarID } = require('../validators/idValidator')
+const { validarCargo } = require('../validators/CargoValidator')
+const { validarFuncionario } = require('../validators/FuncionarioValidator')
+const { validarDepartamento } = require('../validators/DepartamentoValidator')
 
 // Cargo
-router.post('/cargos', validarCargo, CargoController.criar);
-router.get('/cargos', CargoController.buscarTodos);
-router.get('/cargos/:id', validarID, CargoController.buscarPorID);
-router.put('/cargos/:id', validarID, validarCargo, CargoController.atualizar);
-router.delete('/cargos/:id', validarID, CargoController.excluir);
+router.post('/cargos', validarCargo, CargoController.criar)
+router.get('/cargos', CargoController.buscarTodos)
+router.get('/cargos/:id', validarID, CargoController.buscarPorID)
+router.put('/cargos/:id', validarID, validarCargo, CargoController.atualizar)
+router.delete('/cargos/:id', validarID, CargoController.excluir)
 
 // Funcionario
-router.post('/funcionarios', validarFuncionario, FuncionarioController.criar);
-router.get('/funcionarios', FuncionarioController.buscarTodos);
-router.get('/funcionarios/:id', validarID, FuncionarioController.buscarPorID);
-router.put('/funcionarios/:id', validarID, validarFuncionario, FuncionarioController.atualizar);
-router.delete('/funcionarios/:id', validarID, FuncionarioController.excluir);
+router.post('/funcionarios', validarFuncionario, FuncionarioController.criar)
+router.get('/funcionarios', FuncionarioController.buscarTodos)
+router.get('/funcionarios/:id', validarID, FuncionarioController.buscarPorID)
+router.put('/funcionarios/:id', validarID, validarFuncionario, FuncionarioController.atualizar)
+router.delete('/funcionarios/:id', validarID, FuncionarioController.excluir)
 
-// Departamentos (adicionar as rotas conforme necessário)
+// Departamentos
+router.get('/departamentos', DepartamentoController.buscarTodos)
+router.get('/departamentos/:id', validarID, DepartamentoController.buscarPorID)
+router.post('/departamentos', validarDepartamento, DepartamentoController.criar)
+router.put('/departamentos/:id', validarID, validarDepartamento, DepartamentoController.atualizar)
+router.delete('/departamentos/:id', validarID, DepartamentoController.excluir)
 
 module.exports = router;
